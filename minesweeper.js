d395b926f50c852054e1d2b0386ec4f8d3353049
Cliff Crosland <cliffcrosland@gmail.com>
2012-10-31 01:02:42 -0700

    Begin creating GameGridModel and SolverGridModel

--- minesweeper.js
+++ minesweeper.js
@@ -1,13 +1,13 @@
 (function () {
-  var NUM_ROWS = 10, NUM_COLS = 10;
+  var NUM_ROWS = 10, NUM_COLS = 10, NUM_BOMBS = 25, BOMB_STRING = "B";
 
-  var gameGrid = null, solverGrid = null;
+  var gameGridModel = null, solverGridModel = null;
 
   $(init);
 
   function init() {
-    gameGrid = emptyGrid(NUM_ROWS, NUM_COLS);
-    solverGrid = emptyGrid(NUM_ROWS, NUM_COLS);
+    gameGridModel = new GameGridModel(NUM_ROWS, NUM_COLS, NUM_BOMBS);
+    solverGridModel = new SolverGridModel(NUM_ROWS, NUM_COLS, gameGridModel);
 
     initUI();
   }
@@ -16,7 +16,47 @@
   // Model
   ////////////////
 
-  function emptyGrid(numRows, numCols) {
+  function GameGridModel(numRows, numCols, numBombs) {
+    var self = this;
+
+    self.numBombs = numBombs;
+    self.data = emptyDataGrid(numRows, numCols);
+
+    self.get = function(row, col) {
+      return self.data[row][col];
+    };
+
+    self.set = function(row, col, val) {
+      self.data[row][col] = val;
+    };
+
+    self.init = function() {
+      // Place bombs randomly
+      var coords = [], numRows = self.data.length, numCols = self.data[0].length;
+      for (var row = 0; row < numRows; row++) {
+        for (var col = 0; col < numCols; col++) {
+          coords.push([row, col]);
+        }
+      }
+      coords = _.shuffle(coords);
+      for (var bomb = 0; bomb < self.numBombs; bomb++) {
+        var bombCoord = coords[bomb];
+        self.set(bombCoord[0], bombCoord[1], BOMB_STRING);
+      }
+    };
+
+    self.init();
+  }
+
+  function SolverGridModel(numRows, numCols, gameGridModel) {
+    var self = this;
+    // TODO: implement
+    self.get = function(row, col) {
+      return "hi";
+    }
+  }
+
+  function emptyDataGrid(numRows, numCols) {
     var ret = new Array(numRows);
     for (var row = 0; row < numRows; row++) {
       ret[row] = new Array(numCols);
@@ -31,18 +71,18 @@
   // UI
   ////////////////
   function initUI() {
-    createTableLayout($("#game"), gameGrid);
-    createTableLayout($("#solver"), solverGrid);
+    createTableLayout($("#game"), gameGridModel);
+    createTableLayout($("#solver"), solverGridModel);
 
     $("#game").on("click", "td", gameCellClickHandler);
   }
 
-  function createTableLayout($container, data) {
+  function createTableLayout($container, dataModel) {
     var ret = "<table>";
     for (var row = 0; row < NUM_ROWS; row++) {
       ret += "<tr>";
       for (var col = 0; col < NUM_COLS; col++) {
-        ret += "<td>" + data[row][col] + "</td>";
+        ret += "<td>" + dataModel.get(row, col) + "</td>";
       }
       ret += "</tr>";
     }
